304a70586308b8700c8215bbc6628aace9cc6d22
tjdeveloper <tjmr1996@gmail.com>
2023-07-27 19:45:54 +0500

    error in prod

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -15,7 +15,7 @@ const sendErrorDev = (err, req, res) => {
 const sendErrorProd = (err, req, res) => {
   res.status(err.statusCode).render('error', {
     pageTitle: 'Xatolik!',
-    msg: "err.message",
+    msg: err.message,
   });
 };
 export const globalErrorHandler = (err, req, res, next) => {
